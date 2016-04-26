a7c88c4a8fb53172efdce8ac5cf662a7ba585913
Roman Ustuantsev <liberrom.hse@gmail.com>
2016-04-27 00:06:22 +0300

    added initial files

---
+++ ZombieConga/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  ZombieConga
+//
+//  Created by Roman Ustiantcev on 26/04/16.
+//  Copyright (c) 2016 Roman Ustiantcev. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+   
+    override func didMoveToView(view: SKView) {
+        
+        backgroundColor = SKColor.blackColor()
+        
+    }
+    
+    
+}
---
+++ ZombieConga/GameViewController.swift
@@ -0,0 +1,33 @@
+//
+//  GameViewController.swift
+//  ZombieConga
+//
+//  Created by Roman Ustiantcev on 26/04/16.
+//  Copyright (c) 2016 Roman Ustiantcev. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene(size: CGSize(width: 2048, height: 1536))
+        let skView = self.view as! SKView
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        
+        scene.scaleMode = .AspectFill
+        
+        skView.presentScene(scene)
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
+}
